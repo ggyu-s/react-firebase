f9fc128e85b514918eea9b181049f55495ad92a9
JungGYUCHEOL <kcpooh0202@naver.com>
2021-02-17 16:54:02 +0900

    firebase

---
+++ reactfirebase/src/routes/Home.js
@@ -0,0 +1,67 @@
+import { dbService } from "fbase";
+import React, { useEffect, useState } from "react";
+
+const Home = ({ userObj }) => {
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  //   const getNweets = async () => {
+  //     const dbnweets = await dbService.collection("nweets").get();
+  //     dbnweets.forEach((document) => {
+  //       const nweetObject = {
+  //         ...document.data(),
+  //         id: document.id,
+  //       };
+  //       setNweets((prev) => [nweetObject, ...prev]);
+  //     });
+  //   };
+
+  useEffect(() => {
+    // getNweets();
+    dbService.collection("nweets").onSnapshot((snapshot) => {
+      const nweetArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setNweets(nweetArray);
+    });
+  }, []);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await dbService.collection("nweets").add({
+      text: nweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+    });
+    setNweet("");
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+          onChange={onChange}
+          value={nweet}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.text}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
